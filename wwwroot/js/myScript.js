72929b77d1c2b701be467bce375a02f20f0944a9
laytonbarney <64536515+laytonbarney@users.noreply.github.com>
2022-01-10 17:36:51 -0700

    commit
    commit

---
+++ wwwroot/js/myScript.js
@@ -0,0 +1,10 @@
+﻿$("#btnSend").click(function () {
+
+    var as = $("#assignements").val();
+    var qu = $("#quizzes").val();
+    var i = $("#intex").val();
+    var ex = $("#extracredit").val();
+
+    var result = (as*.4367)+(qu*.131)+(i*.4323)+(ex*.022)
+    alert("You would score " + result +"% in this class");
+})
